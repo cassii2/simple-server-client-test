b474cc9a71055c22d4ffade7f6672ce9510c7480
cassii2 <bradii2spencer@gmail.com>
2022-02-27 20:33:45 -0800

    message for when i go to my laptop

--- example.sh
+++ example.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+# "_POSIX_C_SOURCE=200112L" in defines in c_cpp_properties.json
 
 ./main 30630 "$(echo -e "HTTP/1.1 200 OK\nContent-Type: text/html\n\n<html>\n\t<h1>hi</h1>\n</html>\n\ngarbage\nasdgl")"
