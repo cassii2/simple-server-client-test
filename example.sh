df6bf7fbc325a604a94c5d7d308a59c181aa58c6
cassii2 <bradii2spencer@gmail.com>
2022-03-10 20:17:58 -0800

    remove old project files

--- example.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# "_POSIX_C_SOURCE=200112L" in defines in c_cpp_properties.json
-
-./scripted 30630 "$(echo -e "HTTP/1.1 200 OK\nContent-Type: text/html\n\n<html>\n\t<h1>hi</h1>\n</html>\n\ngarbage\nasdgl")"
